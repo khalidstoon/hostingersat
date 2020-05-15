a72193ee61c9b2ea26bf8a764ee0a8db9475c1dc
khalidstoon <65396500+khalidstoon@users.noreply.github.com>
2020-05-15 13:08:43 +0000

    Update tikchbila.js

--- tikchbila.js
+++ tikchbila.js
@@ -1173,11 +1173,11 @@
     /* no string id? so it's object params, and set the id with default id */
     if (typeof(tag_id) != 'string') {
       params = tag_id;
-      tag_id = 'test';
+      tag_id = 'site-footer';
     }
     /* no id? set the id to default id */
     if (!tag_id) {
-      tag_id = 'test';
+      tag_id = 'site-footer';
     }
     /* pJS elements */
     var pJS_tag = document.getElementById(tag_id),
